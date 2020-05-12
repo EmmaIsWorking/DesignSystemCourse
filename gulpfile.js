07641cd72b473ae27d670fda091e23f5ab51a5fb
EmmaIsWorking <dimple.pleod@gmail.com>
2020-05-12 17:04:38 -0500

    change gulp for a version more old

--- gulpfile.js
+++ gulpfile.js
@@ -5,39 +5,37 @@ var minifyCSS = require('gulp-minify-css');
 var version = require('./package.json');
 
 //npm install gulp --save --only=dev
-gulp.task('hello', async () => {
+gulp.task('hello', function () {
   console.log('Hello ');
 });
+
 //npm install gulp-sass
 
-gulp.task('sass', async () => {
-  return gulp
-    .src('scss/**/*.scss')
+gulp.task('sass', function () {
+  return gulp.src('scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('public/stylesheets'));
+    .pipe(gulp.dest('public/stylesheets'))
 });
 
 //npm install gulp-concat
 
-gulp.task('style', async () => {
-  return gulp
-    .src('scss/**/*.scss')
+gulp.task('style', function () {
+  return gulp.src('scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(concat('style_main.css'))
     .pipe(gulp.dest('public/stylesheets'))
 });
 
 
-// //npm install gulp-minify-css
+//npm install gulp-minify-css
 
-// gulp.task('style_min', async () => {
-//   return gulp
-//     .src('scss/**/*.scss')
-//     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-//     .pipe(minifyCSS())
-//     .pipe(concat('style_main_' + version.version + '.min.css'))
-//     .pipe(gulp.dest('public/stylesheets'))
-// });
+gulp.task('style_min', function () {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(minifyCSS())
+    .pipe(concat('style_main_' + version.version + '.min.css'))
+    .pipe(gulp.dest('public/stylesheets'))
+});
 
 //gulp watch
 
